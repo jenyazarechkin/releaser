c5696922b557ebdede3ccc8a94848043a680ecda
Jenya Zarechkin <yes@quasaragency.ru>
2015-12-09 16:49:46 +0500

    Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,13 @@
 class DashboardController < ApplicationController
 
-
-
   def index
+    @team = params['team'] ? Team.find(params['team']): Team.order('name').first
     @teams = Team.preload(:team_members).order(:name).all
+    if(@team.releases.present?)
+      @release= @team.releases.order('released_at desc').first
+      @steps_releases = @release.steps_releases.where(is_enabled: true).preload(:step).all
+      @countries_releases = @release.countries_releases.where(is_enabled: true).preload(:country).all
+    end
   end
 
 end
